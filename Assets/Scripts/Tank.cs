477fc99e852bea1b9042377536a8365e9de7bd3a
kyuuzou <newita@gmail.com>
2023-06-18 22:51:46 +0100

    Improve player movement.

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -2,6 +2,31 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour {
 
+    [Header("Input")]
+    [SerializeField]
+    private string fireAxis = "Fire1";
+
+    [SerializeField]
+    private string horizontalAxis = "Horizontal";
+
+    [SerializeField]
+    private string verticalAxis = "Vertical";
+
+    [Header("Movement: Horizontal")]
+    [SerializeField]
+    private float angularAcceleration = 10.0f;
+
+    [SerializeField]
+    private float maximumTurnSpeed = 1.0f;
+
+    [Header("Movement: Vertical")]
+    [SerializeField]
+    private float acceleration = 10.0f;
+
+    [SerializeField]
+    private float maximumSpeed = 20.0f;
+
+    [Header("World")]
     [SerializeField]
     private Transform actorRoot;
 
@@ -9,35 +34,44 @@ public class Tank : MonoBehaviour {
     private Projectile projectilePrefab;
 
     [SerializeField]
-    private float speed = 10.0f;
-
-    [SerializeField]
     private Transform visuals;
 
-    private Vector3 direction;
+    private Vector2 movementInput;
     private new Rigidbody rigidbody;
-
-    private void AdjustVisuals() {
-        this.visuals.LookAt(this.visuals.position + this.direction);
-    }
 
     private void Awake() {
         this.rigidbody = this.GetComponent<Rigidbody>();
-        this.direction = Vector3.forward;
     }
 
-    private void HandleMovement() {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        movement *= Time.deltaTime * speed;
-        this.rigidbody.transform.Translate(movement);
+    private void FixedUpdate() {
+        if (!Mathf.Approximately(this.movementInput.y, 0.0f)) {
+            Vector3 localVelocity = this.transform.InverseTransformDirection(this.rigidbody.velocity);
 
-        if (!Mathf.Approximately(movement.magnitude, 0.0f)) {
-            this.direction = movement.normalized;
+            if (localVelocity.z < maximumSpeed && localVelocity.z > -maximumSpeed){
+                this.rigidbody.AddForce(
+                    this.transform.forward * this.movementInput.y * acceleration * this.rigidbody.mass
+                );
+            }
+        }
+
+        if (! Mathf.Approximately(this.movementInput.x, 0.0f)) {
+            if (this.rigidbody.angularVelocity.magnitude < this.maximumTurnSpeed) {
+                // invert turn direction when we're moving backwards
+                float direction = Mathf.Sign(this.movementInput.y);
+
+                this.rigidbody.AddTorque(
+                    this.transform.up * this.movementInput.x * this.angularAcceleration * this.rigidbody.mass * direction
+                );
+            }
         }
     }
 
-    private void HandleShooting() {
-        if (Input.GetButtonDown("Fire1")) {
+    private void HandleMovementInput() {
+        this.movementInput.Set(Input.GetAxis(this.horizontalAxis), Input.GetAxis(this.verticalAxis));
+    }
+
+    private void HandleShootingInput() {
+        if (Input.GetButtonDown(this.fireAxis)) {
             Projectile projectile = Object.Instantiate<Projectile>(
                 this.projectilePrefab,
                 this.transform.position + this.projectilePrefab.transform.position,
@@ -47,12 +81,8 @@ public class Tank : MonoBehaviour {
         }
     }
 
-    private void LateUpdate() {
-        this.AdjustVisuals();
-    }
-
     private void Update() {
-        this.HandleMovement();
-        this.HandleShooting();
+        this.HandleMovementInput();
+        this.HandleShootingInput();
     }
 }
